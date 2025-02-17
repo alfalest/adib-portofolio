65c3f616285d3adc57d58ccc63b1553ad840e082
Adib Alfaini Afifi <alfainiaff@gmail.com>
2025-02-17 15:58:33 +0700

    Update index.tsx

--- src/features/work/index.tsx
+++ src/features/work/index.tsx
@@ -4,9 +4,17 @@ import WorkExperience from "./components/works.experience";
 const ExperienceSection = () => {
   const workExperiences = [
     {
+      company: "PT Heiszco Atarasindo",
+      title: "Full Time - Full Stack Developer",
+      date: "Feb 2025 - Present",
+      icon: (
+        <BriefcaseBusiness className="h-6 w-6 text-gray-500 dark:text-white" />
+      ),
+    },
+    {
       company: "PT Elixer Reka Digita",
-      title: "Full Stack Developer",
-      date: "Dec 2024 - Present",
+      title: "Freelance - Full Stack Developer",
+      date: "Dec 2024 - Feb 2025",
       location: "Hybrid",
       icon: (
         <BriefcaseBusiness className="h-6 w-6 text-gray-500 dark:text-white" />
@@ -14,7 +22,7 @@ const ExperienceSection = () => {
     },
     {
       company: "PT Aneka Dasuib Jaya",
-      title: "Tech Lead - Full Stack Developer",
+      title: "Full Time - Full Stack Developer",
       date: "Aug 2023 - Dec 2024",
       icon: (
         <BriefcaseBusiness className="h-6 w-6 text-gray-500 dark:text-white" />
@@ -22,7 +30,7 @@ const ExperienceSection = () => {
     },
     {
       company: "PT Latifah Medika Alkesindo",
-      title: "IT Support",
+      title: "Full Time - IT Support",
       date: "Feb 2023 - July 2023",
       icon: (
         <BriefcaseBusiness className="h-6 w-6 text-gray-500 dark:text-white" />
@@ -30,7 +38,7 @@ const ExperienceSection = () => {
     },
     {
       company: "CV Berdaring Indonesia",
-      title: "Full Stack Developer",
+      title: "Full Time - Full Stack Developer",
       date: "Jan 2022 - Feb 2023",
       location: "Hybrid",
       icon: (
@@ -39,7 +47,7 @@ const ExperienceSection = () => {
     },
     {
       company: "PT Dessa Integrasi Globalindo",
-      title: "Web Developer",
+      title: "Freelance - Web Developer",
       date: "Jan 2021 - Dec 2021",
       location: "Work From Home",
       icon: (
